19526c497c4b7983a089a98beb1cf23066ab1e21
tlgus <a01097190079@gmail.com>
2022-03-29 20:25:07 +0900

    upload

--- ch1/q1.py
+++ ch1/q1.py
@@ -1,1 +1,12 @@
-print('a')
+n, k = map(int, '6 3'.split())
+
+cnt = 0
+
+for i in range(1, n+1) :
+    if n % i == 0 : # i가 n의 약수에 해당
+        cnt += 1
+    if cnt == k : # k번째 약수
+        print(i)
+        break # 반복문을 더 이상 돌릴 이유가 없음
+else : # python : for ~ else
+    print(-1) # 6의 5번째 약수 같은 건 없으니까 이럴 경우 -1 출력
