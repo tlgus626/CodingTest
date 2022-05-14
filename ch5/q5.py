5a756ad7fd8f680b0c6cb4dce2111e5ff53866a2
tlgus <a01097190079@gmail.com>
2022-05-14 13:46:28 +0900

    upload

--- ch5/q5.py
+++ ch5/q5.py
@@ -11,7 +11,7 @@ def DFS(v):
                 SUM += dogs[i]
         if SUM > C : # edge cut
             return
-        if SUM > MAX : # MAX는 지역변수인데, 함수 내에서 참조하고 있음
+        if SUM > MAX : # MAX는 지역변수인데, 함수 내에서 참조하고 있음 (update되는 값이라서)
             MAX = SUM
     else:
         # 바둑이 태움
